05d3560bb526e9132f35256a092cfa76cc9f9716
ikyusix <ikyusixadam@gmail.com>
2020-06-20 09:19:32 +0700

    Initial commit

---
+++ app/src/main/java/com/enigma/lpua_front_end/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.enigma.lpua_front_end
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
